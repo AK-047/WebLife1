54748b7be5a0f84c1bb3ade87193e3ab15e5ef17
Nesk8er <pavkalashnikov@gmail.com>
2014-05-05 01:36:19 +0300

    Added multi languages

---
+++ Helpers/MultiLanguageController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebLife.Helpers
+{
+    public abstract class MultiLanguageController : Controller
+    {
+        protected override void ExecuteCore()
+        {
+            string cultureName = null;
+            string Language = null;
+            HttpCookie cultureCookie = Request.Cookies["Arsenal"];
+            if (cultureCookie != null)
+            {
+                cultureName = cultureCookie.Value;
+            }
+            else
+            {
+                cultureName = "en";
+            }
+
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(cultureName);
+            Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+            base.ExecuteCore();
+        }
+        protected override bool DisableAsyncSupport
+        {
+            get { return true; }
+        }
+	}
+}
