e181376c3dbc5f4d063f7d913d1dcfc53a39912a
Nesk8er <pavkalashnikov@gmail.com>
2014-05-05 00:12:08 +0300

    First

---
+++ DAL/DataManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.Ajax.Utilities;
+using WebLife.Models;
+
+namespace WebLife.DAL
+{
+    public class DataManager
+    {
+        public DataManager()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        private ApplicationDbContext context;
+
+        public ApplicationUser GetUser(Func<ApplicationUser,bool> predicate)
+        {
+            return context.Users.SingleOrDefault(predicate);
+        }
+
+        public void ModifyUser(ApplicationUser user)
+        {
+            ApplicationUser oldUser = GetUser(u => u.Id == user.Id);
+            oldUser.ConfirmationToken = user.ConfirmationToken;
+            oldUser.IsConfirmed = user.IsConfirmed;
+            oldUser.Email = user.Email;
+            DbSet<ApplicationUser> dbSet = context.Set<ApplicationUser>();
+            dbSet.Attach(oldUser);
+            context.Entry(oldUser).State = EntityState.Modified;
+            context.SaveChanges();
+
+        }
+
+    }
+}
